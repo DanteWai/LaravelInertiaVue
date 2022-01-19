e5cf08449a4ed88a726278bede6a3174f5b08e55
Dmitry Safronov <aa502@yandex.ru>
2022-01-19 20:45:37 +0300

    Инициализация инерции на фронте

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
